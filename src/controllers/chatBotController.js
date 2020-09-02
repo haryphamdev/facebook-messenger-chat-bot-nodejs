24b95055c01587a500634b569d7054f81640e249
Pham Tuan <jvgiveup@gmail.com>
2020-09-02 16:26:41 +0700

    update entity names

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -154,8 +154,12 @@ function callSendAPI(sender_psid, response) {
     });
 }
 
-function firstEntity(nlp, name) {
-    return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
+// function firstTrait(nlp, name) {
+//     return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
+// }
+
+function firstTrait(nlp, name) {
+    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
 }
 
 function handleMessage(sender_psid, message) {
@@ -168,10 +172,10 @@ function handleMessage(sender_psid, message) {
         return;
     }
 
-    let entitiesArr = [ "greetings", "thanks", "bye" ];
+    let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
     let entityChosen = "";
     entitiesArr.forEach((name) => {
-        let entity = firstEntity(message.nlp, name);
+        let entity = firstTrait(message.nlp, name);
         if (entity && entity.confidence > 0.8) {
             entityChosen = name;
         }
@@ -181,15 +185,15 @@ function handleMessage(sender_psid, message) {
         //default
         callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
     }else{
-       if(entityChosen === "greetings"){
+       if(entityChosen === "wit$greetings"){
            //send greetings message
            callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
        }
-       if(entityChosen === "thanks"){
+       if(entityChosen === "wit$thanks"){
            //send thanks message
            callSendAPI(sender_psid,`You 're welcome!`);
        }
-        if(entityChosen === "bye"){
+        if(entityChosen === "wit$bye"){
             //send bye message
             callSendAPI(sender_psid,'bye-bye!');
         }
